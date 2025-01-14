2562e0c4151f6e7cc2deef67c66e021cadd05a82
Sagarjana9883 <janajana721438@gmail.com>
2025-01-14 21:10:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    let display = document.getElementById('display').value;
+    let result = eval(display);
+    document.getElementById('display').value = result;
+}
